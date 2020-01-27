8a83364a07ea6691c7b94fba54e1772e6e743906
fatclarence <ckoh.clarence.96@gmail.com>
2020-01-28 01:15:56 +0800

    add set bomb input for keyboard

---
+++ Assets/Scripts/Gameplay/SetBomb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SetBomb : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bombPrefab;
+    private void Start()
+    {
+        AetherInput.GetPlayerActions().SetBomb.performed += HandleSetBomb;
+    }
+
+    private void HandleSetBomb(InputAction.CallbackContext obj)
+    {
+        // Hold B on keyboard for 2 seconds, instantiate bomb-mushroom
+        Instantiate(_bombPrefab, transform.position, transform.rotation);
+    }
+}
