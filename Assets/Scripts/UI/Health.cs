cdc84588b85674b20f0bd2a42c918a694a065fbb
fatclarence <ckoh.clarence.96@gmail.com>
2020-01-27 21:49:34 +0800

    add healthbar to player prefab

---
+++ Assets/Scripts/UI/Health.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int m_MaxHealth = 100;
+
+    private int m_CurrentHealth;
+    public event Action<float> OnHealthPercentageChanged = delegate { };
+
+    private void OnEnable()
+    {
+        m_CurrentHealth = m_MaxHealth;
+    }
+
+    public void ModifyHealth(int amount)
+    {
+        m_CurrentHealth += amount;
+        float currentHealthPercent = (float)m_CurrentHealth / (float)m_MaxHealth;
+        OnHealthPercentageChanged(currentHealthPercent);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("z"))
+        {
+            ModifyHealth(-10);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_HealthRedBar;
+    [SerializeField]
+    private float m_UpdateSpeedSeconds = 0.5f;
+    [SerializeField]
+    private Camera m_MainCam;
+
+    private void Awake()
+    {
+        GetComponentInParent<Health>().OnHealthPercentageChanged += HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(float percent)
+    {
+        StartCoroutine(ChangeToPercent(percent));
+    }
+
+    private IEnumerator ChangeToPercent(float newHealthPercent)
+    {
+        float currentPercentage = m_HealthRedBar.fillAmount;
+        float timeElapsed = 0f;
+
+        while(timeElapsed < m_UpdateSpeedSeconds)
+        {
+            timeElapsed += Time.deltaTime;
+            m_HealthRedBar.fillAmount = Mathf.Lerp(currentPercentage, newHealthPercent, timeElapsed / m_UpdateSpeedSeconds);
+            yield return null;
+        }
+        // Ensure that the final intended health percentage is reached.
+        m_HealthRedBar.fillAmount = newHealthPercent;
+    }
+
+    private void LateUpdate()
+    {
+        this.gameObject.transform.position = this.gameObject.transform.parent.transform.position + Vector3.up * 2.5f;
+        // Rotate healthbar towards viewing camera
+        transform.LookAt(m_MainCam.transform);
+        transform.Rotate(0, 180, 0);
+    }
+}
