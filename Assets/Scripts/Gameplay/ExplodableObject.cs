d6cd02682baa71a52ec956a1965c2c6e789abcca
fatclarence <ckoh.clarence.96@gmail.com>
2020-02-03 03:14:15 +0800

    add exploding object scripts and fall animation

---
+++ Assets/Scripts/Gameplay/ExplodableObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodableObject : MonoBehaviour
+{
+    private float m_TerrainRevealRadius = 30.0f;
+    private static float m_TimeOfLastSFX = 0.0f;
+
+    private int timeDelay = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject, timeDelay);
+    }
+
+    void OnDestroy()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_TerrainRevealRadius);
+
+        foreach (Collider c in colliders)
+        {
+            RevealableTerrain target = c.GetComponent<RevealableTerrain>();
+
+            if (target != null)
+            {
+                target.PaintAtPosition(transform.position, m_TerrainRevealRadius);
+            }
+
+            RevealableObject theObject = c.GetComponent<RevealableObject>();
+
+            if (theObject != null)
+            {
+                theObject.Reveal();
+            }
+
+            FallAnimation anim = c.GetComponent<FallAnimation>();
+
+            if (anim != null)
+            {
+                anim.MakeCharacterFall();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/FallAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_Animator;
+
+    public void MakeCharacterFall()
+    {
+        StartCoroutine("FallAction");
+    }
+
+    IEnumerator FallAction()
+    {
+        m_Animator.SetTrigger("Fallen");
+        yield return new WaitForSeconds(1);
+        m_Animator.ResetTrigger("Fallen");
+    }
+}
