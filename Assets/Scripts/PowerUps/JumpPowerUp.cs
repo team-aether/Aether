2982b9f5fc594b94357288b53a1fb8a8682b573b
fatclarence <ckoh.clarence.96@gmail.com>
2020-02-03 09:26:02 +0800

    add sean's powerup scripts without textchanger

---
+++ Assets/Scripts/PowerUps/JumpPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPowerUp : MonoBehaviour
+{
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Player"))
+        {
+            PowerUpsManager manager = c.GetComponent<PowerUpsManager>();
+
+            if (manager != null && !manager.GetDoubleJump())
+            {
+                manager.JumpHigher();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
