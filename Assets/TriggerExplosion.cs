60906a4d61025f1257ddfcc8ede0bbd7dc9e2945
fatclarence <ckoh.clarence.96@gmail.com>
2020-02-13 15:34:31 +0800

    add skeletal meteor projectile effect

---
+++ Assets/TriggerExplosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerExplosion : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Destruction;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Meteor"))
+        {
+            Debug.Log("METEORRRRR");
+            GameObject meteorHit = Instantiate(m_Destruction, other.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(meteorHit, 1.5f);
+        }
+    }
+}
