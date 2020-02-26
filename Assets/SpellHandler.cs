a7ad4a22c82388962603ec5550e4b2561b9fcef6
fatclarence <ckoh.clarence.96@gmail.com>
2020-02-27 01:15:02 +0800

    create prototype basic spell

---
+++ Assets/SpellHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpellHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_SpellPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AetherInput.GetPlayerActions().SpellCast.performed += HandleSpell;
+    }
+
+    // Update is called once per frame
+    void HandleSpell(InputAction.CallbackContext ctx)
+    {
+        m_SpellPrefab.SetActive(true);
+        m_SpellPrefab.GetComponent<ParticleSystem>().Play();
+    }
+}
